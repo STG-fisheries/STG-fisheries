3af24de9f0bd4c129b36f21e05a4327871840a55
Spencer Gardner <85301081+STG-fisheries@users.noreply.github.com>
2021-06-25 10:56:03 -0400

    Update power_analysis.R

--- power_analysis.R
+++ power_analysis.R
@@ -1,12 +1,21 @@
-## ------ Source:   https://slcladal.github.io/pwr.html#Introduction ------ ##
 ## ------ Title:    Experimental Power Analysis in R ------ ## 
 ## ------ Editor:   Spencer T. Gardner ------ ##
 ## ------ Date:     6/23/2021 ------ ##
 
-# What determines if your model finds an effect?
+# Type I Error (alpha) = Probability of incorrectly rejecting a true null hypothesis 
+#       - This is a FALSE POSITIVE, concluding there's a pattern when one doesn't exist
+#       - alpha is the acceptable significance level (usually set to 0.05 or 5%)
+
+# Type II Error (beta) = Probability of failing to reject a false null hypothesis (false negative)
+#       - This is a FALSE NEGATIVE, concluding there's no pattern when one actually exist
+#       - (1 - beta) is the statistical power... 
+
+# What determines statistical power?
 #         - Size of the effect
 #         - Variabiltiy in the effect (sd)
 #         - Sample size (n) ... number of observable replicates
+
+# There is a TRADE-OFF between Type I and Type II errors. Decreasing the probability of one, increases probability of the other. 
 
 #   Before conducting an experiment we should always determine how likely we are 
 # to detect an effect given our experimental design (i.e., replicates, treatment
